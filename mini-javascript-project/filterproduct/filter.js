a92e969ddba71e2a6c69eef9d0cba75a152a8e4d
Ansh <anshpathania@gmail.com>
2024-10-02 19:50:33 +0530

    fiter product project done

--- mini-javascript-project/filterproduct/filter.js
+++ mini-javascript-project/filterproduct/filter.js
@@ -6,12 +6,12 @@ const categoryBtns = document.querySelectorAll('.category-btn');
 function filterProduct(){
  const SearchValue = searchInput.value.toLowerCase();
  const productItem = document.querySelectorAll(".product-item");
- 
+ const activeCategory = document.querySelector(".category-btn.active").dataset.category;
  productItem.forEach(item =>{
      const title = item.querySelector('h3').innerText.toLowerCase();
      const category = item.dataset.category;
      
-     if((title.includes(SearchValue) || SearchValue == "") ){
+     if((title.includes(SearchValue) || SearchValue == "") && (category === activeCategory || activeCategory ==="all") ){
          item.style.display="inline-block";
         }
         else {
@@ -20,45 +20,25 @@ function filterProduct(){
         }
         
         
-        // && (category === activeCategory || activeCategory ==="all")
+        //
         
         
     })
 }
 
-function categoryProduct(){
-     const activeCategory = document.querySelector(".category-btn.active").dataset.category;
-     const SearchValue = searchInput.value.toLowerCase();
- const productItem = document.querySelectorAll(".product-item");
- 
- productItem.forEach(item =>{
-     const title = item.querySelector('h3').innerText.toLowerCase();
-     const category = item.dataset.category;
-     
-     if((title.includes(SearchValue) || SearchValue == "")&& (category === activeCategory || activeCategory ==="all") ){
-         item.style.display="inline-block";
-        }
-        else {
-            item.style.display="none";
-            
-        }
-        
-        
-        // 
-        
-        
-    })
-}
 
 function setCategory(e){
     categoryBtns.forEach(btn =>{
         btn.classList.remove("active");
         e.target.classList.add("active");
-       categoryProduct();
+    filterProduct();
     })
 
 }
 
+// Select the first category button by default (assuming buttons have class names)
+const firstCategoryBtn = categoryBtns[0];
+firstCategoryBtn.classList.add("active"); // Set the first button as active
 
 searchBtn.addEventListener("click",filterProduct);
 searchInput.addEventListener("keyup",filterProduct);
